8a37fe045a32ba57745fa180aeb11dad29d9deed
houle-LHp5 <houle-LHp5@users.noreply.github.com>
2021-01-19 16:54:48 +0100

    modification finale du 19/01/2021

---
+++ index.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <title>KanysPhotoGraphie</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="cover.css" rel="stylesheet">
+</head>
+<body class="overflow-hidden">
+    <thead>
+        <div class="containeur-fluid">
+            <!--Navbar -->
+            <nav class="mb-1 navbar navbar-expand-lg navbar-dark fixed-top" id="navbarPageAccueil">
+                <a class="navbar-brand text-white" href="index.php">KANY'S</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="pageAccueil">
+                                <i class="fab fa-instagram"></i>Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="portrait.php">
+                                <i class="fab fa-instagram"></i>Portrait</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="decorationInterieur.php">
+                                <i class="fab fa-instagram"></i>Décoration Intérieur</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">
+                                <i class="fab fa-instagram"></i>Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="aPropos.php">
+                                <i class="fab fa-instagram"></i>A propos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="admin.php">
+                                <i class="fab fa-instagram"></i>Admin</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="tableBord.php">
+                                <i class="fab fa-instagram"></i>Tableau de Bord</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+    </thead>
+    <!--/.Navbar -->
+    <!-- vidéo et message de la page d'accueil -->
+    <div id="videoDiv">
+        <video id="video1" controls class="min-vh-100 min-vw-100" autoplay="" muted="" playsinline="" loop="" poster="assets/videoEtPhotoFondEcran/camera-2125549_1920.jpg">
+            <source src="assets/videoEtPhotoFondEcran/appareilPhotoEntier.mp4" type="video/mp4">
+        </video>
+        <div id="videoMessage" class="styling">
+            <h1 id="titlePageAccueil">KanyPhotography</h1>
+            <h2>Bienvenue sur mon site</h2>
+            <p id="enterBtn" class="videoClick">
+                <a type="submit" class="btn btn-warning btn-lg" href="pageAccueil">Cliquer ici</a>
+            </p>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js">
+    </script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    </div>
+</body>
+</html>
--- pageAccueil.php
+++ pageAccueil.php
@@ -44,106 +44,217 @@
                             <i class="fab fa-instagram"></i>Contact</a>
                     </li>
                     <li class="nav-item">
-                            <a class="nav-link" href="admin.php">
-                                <i class="fab fa-instagram"></i>Admin</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="tableBord.php">
-                                <i class="fab fa-instagram"></i>Tableau de Bord</a>
-                        </li>
+                        <a class="nav-link" href="admin.php">
+                            <i class="fab fa-instagram"></i>Admin</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="tableBord.php">
+                            <i class="fab fa-instagram"></i>Tableau de Bord</a>
+                    </li>
                 </ul>
             </div>
         </nav>
         <!-- fin de la navbar-->
 
-        <!-- premier image de la page touteLesCategories.html -->
- 
+<div class="row d-flex justify-content-center">
+<div class="card" style="width: 18rem">
+  <div class="card-body">
+    <h2 class="card-title">TOURE Aïssétou</h2>
+    <p class="card-text">
+      Bonojour, je suis une jeune auto entrepreneuse de 27ans, j'ai créer mon entreprise en 2019, je sort tout juste de formation de photographie. 
+    </p>
+  </div>
+</div>
+</div>
 
-    <footer class="bg-dark text-white text-center text-lg-start">
-        <!-- Grid container -->
-        <div class="container p-4">
-            <!--Grid row-->
-            <div class="row">
-                <!--Grid column-->
-                <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                    <h5 class="text-uppercase">a propos de Kany's</h5>
-
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                        molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-                        voluptatem veniam, est atque cumque eum delectus sint!
-                    </p>
-                </div>
-                <!--Grid column-->
-
-                <!--Grid column-->
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                    <h5 class="text-uppercase">Links</h5>
-
-                    <ul class="list-unstyled mb-0">
-                        <li>
-                            <a href="#!" class="text-white"></a>
-                            <i class="fab fa-facebook"></i>
-                        </li>
-                        <li>
-                            <a href="#!" class="text-white"></a>
-                        </li>
-                        <li>
-                            <a href="#!" class="text-white"></a>
-                        </li>
-                        <li>
-                            <a href="#!" class="text-white"></a>
-                        </li>
-                    </ul>
-                </div>
-                <!--Grid column-->
-
-                <!--Grid column-->
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                    <h5 class="text-uppercase mb-0">Links</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#!" class="text-white"></a>
-                        </li>
-                        <li>
-                            <a href="#!" class="text-white"></a>
-                        </li>
-                        <li>
-                            <a href="#!" class="text-white"></a>
-                        </li>
-                        <li>
-                            <a href="#!" class="text-white"></a>
-                        </li>
-                    </ul>
-                </div>
-                <!--Grid column-->
+        <div class="">
+            <div class="text-center">
+                <h1>Actualité</h1>
+                <p class="">"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur.
+                    Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum."
+                </p>
             </div>
-            <!--Grid row-->
-        </div>
-        <!-- Grid container -->
-        <i class="fab fa-facebook"></i>
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-            © 2021 Copyright: KanyPhotography
-            <a class="text-white" href="https://mdbootstrap.com/"></a>
-        </div>
-        <!-- Copyright -->
-    </footer>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js">
-    </script>
-    <script type="text/javascript" src="js/materialize.min.js"></script>
-    <script src="script.js"></script>
+            <div class="row d-flex justify-content-center">
+                <div class="row">
+                    <div class="card p-2 bg-light border" style="width: 18rem">
+                        <img src="assets/img/boysTibab.png" class="card-img-top img-fluid" alt="photo NSNV" />
+                        <div class="card-body">
+                            <p class="card-text">
+                                shooting photo du 10/08/2020 de la marque NSNV (Nul Sacrifice Nul Victoire)
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="card p-2 bg-light border" style="width: 18rem">
+                        <img src="assets/img/kaba.png" class="card-img-top img-fluid" alt="photo NSNV" />
+                        <div class="card-body">
+                            <p class="card-text">
+                                shooting photo du 10/08/2020 de la marque NSNV (Nul Sacrifice Nul Victoire)
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="card p-2 bg-light border" style="width: 18rem">
+                        <img src="assets/img/groupeMannequinStoo.png" class="card-img-top img-fluid" alt="photo NSNV" />
+                        <div class="card-body">
+                            <p class="card-text">
+                                shooting photo du 10/08/2020 de la marque NSNV (Nul Sacrifice Nul Victoire)
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="card p-2 bg-light border" style="width: 18rem">
+                        <img src="assets/img/soeurVoileVoiture.png" class="card-img-top img-fluid" alt="photo NSNV" />
+                        <div class="card-body">
+                            <p class="card-text">
+                                shooting photo du 10/08/2020 de la marque NSNV (Nul Sacrifice Nul Victoire)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="card p-2 bg-light border" style="width: 18rem">
+                        <img src="assets/img/hawaSawaneEtSacopine.png" class="card-img-top img-fluid"
+                            alt="photo NSNV" />
+                        <div class="card-body">
+                            <p class="card-text">
+                                shooting photo du 10/08/2020 de la marque NSNV (Nul Sacrifice Nul Victoire)
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="card p-2 bg-light border" style="width: 18rem">
+                        <img src="assets/img/oumouEtBiggy3.png" class="card-img-top img-fluid" alt="photo NSNV" />
+                        <div class="card-body">
+                            <p class="card-text">
+                                shooting photo du 10/08/2020 de la marque NSNV (Nul Sacrifice Nul Victoire)
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="card p-2 bg-light border" style="width: 18rem">
+                        <img src="assets/img/ourkty2.png" class="card-img-top img-fluid" alt="photo NSNV" />
+                        <div class="card-body">
+                            <p class="card-text">
+                                shooting photo du 10/08/2020 de la marque NSNV (Nul Sacrifice Nul Victoire)
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="card p-2 bg-light border" style="width: 18rem">
+                        <img src="assets/img/soeurVoiture4.png" class="card-img-top img-fluid" alt="photo NSNV" />
+                        <div class="card-body">
+                            <p class="card-text">
+                                shooting photo du 10/08/2020 de la marque NSNV (Nul Sacrifice Nul Victoire)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer id="footerPageAccueil" class="text-white text-center text-lg-start">
+            <!-- Grid container -->
+            <div class="container p-4">
+                <!--Grid row-->
+                <div class="row">
+                    <!--Grid column-->
+                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">a propos de Kany's</h5>
+
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+                            molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+                            voluptatem veniam, est atque cumque eum delectus sint!
+                        </p>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Links</h5>
+
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <a href="#!" class="text-white"></a>
+                                <i class="fab fa-facebook"></i>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white"></a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white"></a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white"></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase mb-0">Links</h5>
+
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!" class="text-white"></a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white"></a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white"></a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white"></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+                </div>
+                <!--Grid row-->
+            </div>
+            <!-- Grid container -->
+            <i class="fab fa-facebook"></i>
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+                © 2021 Copyright: KanyPhotography
+                <a class="text-white" href="https://mdbootstrap.com/"></a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+        </script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js">
+        </script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+        <script src="script.js"></script>
 
 </body>
 
