39b07154de3c4720cc51d85b5079433b5ab8a204
houle-LHp5 <houle-LHp5@users.noreply.github.com>
2021-02-09 14:39:18 +0100

    modification du 09/02/2021

--- decorationInterieur.php
+++ decorationInterieur.php
@@ -163,66 +163,43 @@
         
 
         <footer id="allFooter" class="text-white text-center text-lg-start">
-                <div class="container-fluid p-4">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                            <h5 class="text-uppercase">Footer Content</h5>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                                molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-                                voluptatem veniam, est atque cumque eum delectus sint!
-                            </p>
+                    <div class="container-fluid p-4">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                                <h5 class="text-uppercase">Footer Content</h5>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+                                    molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+                                    voluptatem veniam, est atque cumque eum delectus sint!
+                                </p>
+                            </div>
+                            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                                <h5>Mes réseaux Sociaux</h5>
+                                <div class="row text-center ml-5">
+                                    <div class="ml-5">
+                                        <a href="https://www.facebook.com/toure.aisettou" ><img src="assets/autres/iconesReseauSociaux/Web 1280 – 11.png" class="rounded-circle"></a>
+                                    </div>
+                                    <div class="ml-3">
+                                        <a href="https://www.instagram.com/kanysphotographie/"><img src="assets/autres/iconesReseauSociaux/Web 1280 – 12.png" class="rounded-circle"></a>
+                                    </div>
+                                    <div class="ml-3">
+                                        <a href="#!" ><img src="assets/autres/iconesReseauSociaux/Web 1280 – 13.png" class="rounded-circle"></a>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <!--Grid column-->
                         </div>
-                        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5 class="text-uppercase">Links</h5>
-                            <ul class="list-unstyled mb-0">
-                                <li>
-                                    <i class="fab fa-facebook"></i>
-                                </li>
-                                <li>
-                                    <a href="#!" class="text-white">Link 2</a>
-                                </li>
-                                <li>
-                                    <a href="#!" class="text-white">Link 3</a>
-                                </li>
-                                <li>
-                                    <a href="#!" class="text-white">Link 4</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!--Grid column-->
-
-                        <!--Grid column-->
-                        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5 class="text-uppercase mb-0">Links</h5>
-
-                            <ul class="list-unstyled">
-                                <li>
-                                    <a href="#!" class="text-white">Link 1</a>
-                                </li>
-                                <li>
-                                    <a href="#!" class="text-white">Link 2</a>
-                                </li>
-                                <li>
-                                    <a href="#!" class="text-white">Link 3</a>
-                                </li>
-                                <li>
-                                    <a href="#!" class="text-white">Link 4</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!--Grid column-->
+                        <!--Grid row-->
                     </div>
-                    <!--Grid row-->
-                </div>
-                <!-- Grid container -->
+                    <!-- Grid container -->
 
-                <!-- Copyright -->
-                <div class="text-center p-3">
-                    © 2021 Copyright: KanyPhotography
-                    <a class="text-white" href="https://mdbootstrap.com/"></a>
-                </div>
-            </footer>
+                    <!-- Copyright -->
+                    <div class="text-center p-3">
+                        © 2021 Copyright KanyPhotography
+                        <a class="text-white" href="https://mdbootstrap.com/"></a>
+                    </div>
+                </footer>
 
 
 
