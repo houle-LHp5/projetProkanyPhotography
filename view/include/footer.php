3b5eba7f38a22f7654ba4d298d8c033cc0a9664f
houle-LHp5 <houle-LHp5@users.noreply.github.com>
2021-03-30 12:19:33 +0200

    modification Footer

--- view/include/footer.php
+++ view/include/footer.php
@@ -13,21 +13,40 @@
                             impossible à reproduire" <br />
                             KARL LAGARFELD
                         </p>
+                        <p>
+                            <a href="pageAccueil.php">Accueil</a><br>
+                            <a href="portrait.php">Portrait</a><br>
+                            <a href="decorationInterieur.php">Déco_Intérieure</a><br>
+                            <a href="articles.php">Articles</a><br>
+                            <a href="contact.php">Contact</a>
+                        </p>
+                    </div>
+                    <div>
+                        <div>
+                        <p>Kany's</p>
+                        <p>11 Rue des chemins de Fer</p>
+                        <p>76620 LE HAVRE</p>
+                        <P>Tél : <a href="tel:+33711223344">06 03 80 24 82</a></P>
+                        <P></P>
+                        </div>
+                        
+                    <a href="http://www.monblog.fr/mentions-legales/" target="_blank">Mentions Légales</a>
                     </div>
                     <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
                         <h5>Mes réseaux Sociaux</h5>
                         <div class="row ml-5">
-                            <div class="ml-5 mb-1">
+                            <div class="ml-5 mb-1" style="height: 10rem;" style="width: 10rem;">
                                 <a href="https://www.facebook.com/toure.aisettou"><i class="bi bi-facebook"></i></a>
                             </div>
-                            <div class="ml-3 mb-1">
+                            <div class="ml-3 mb-1" style="height: 10rem;" style="width: 10rem;">
                                 <a href="https://www.instagram.com/kanysphotographie/"><i class="bi bi-instagram text-danger"></i></a>
                             </div>
-                            <div class="ml-3">
+                            <div class="ml-3" style="height: 10rem;" style="width: 10rem;">
                                 <a href="mailto:aisetoutoure@gmail.com"><i class="bi bi-envelope text-warning"></i></a>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
 
